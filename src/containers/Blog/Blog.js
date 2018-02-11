879193ed00a54e691969e9755e4df8653a585adc
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-11 22:10:53 +0100

    using a basic redirect

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,7 @@ import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 class Blog extends Component {
 
@@ -35,6 +35,7 @@ class Blog extends Component {
                 <Switch>
                     <Route path="/new-post" exact component={NewPost} />
                     <Route path="/posts"  component={Posts} />
+                    <Redirect from="/" to="/posts" />
                 </Switch>
             </div>
         );
