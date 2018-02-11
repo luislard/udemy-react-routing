94308728e02840b5605a3d76f3b18a523ba4e992
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-11 17:07:43 +0100

    controlling request error

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,7 +10,8 @@ class Blog extends Component {
 
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount(){
@@ -25,6 +26,10 @@ class Blog extends Component {
                 })
                 this.setState({posts: updatedPosts});
                 // console.log(response);
+            } )
+            .catch(error => {
+               console.log(error); 
+               this.setState({error: true});
             });
     }
 
@@ -33,13 +38,17 @@ class Blog extends Component {
     } 
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                clicked={() => {this.postSelectedHandler(post.id)}}
-                title={post.title} 
-                key={post.id} 
-                author={post.author}/>
-        });
+        let posts = <p style={{textAlign: 'center' }}>Something went wrong!</p>
+        if (!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    clicked={() => {this.postSelectedHandler(post.id)}}
+                    title={post.title} 
+                    key={post.id} 
+                    author={post.author}/>
+            });
+        }
+        
         return (
             <div>
                 <section className="Posts">
