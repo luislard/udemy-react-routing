2938a6fc4d68cc00ac5aa406fc4dd9a1cce29217
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-11 19:15:07 +0100

    moving components, the app is break at this point

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -63,12 +60,7 @@ class Blog extends Component {
                 <section className="Posts">
                     {posts}
                 </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                
             </div>
         );
     }
