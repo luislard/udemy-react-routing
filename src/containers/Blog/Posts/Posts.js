d7ca9465150eefa6885e43d5073add0e1c897401
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-11 19:24:50 +0100

    we are in lecture 174, the app is prepared to handle routing but not yet implemented

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+
+export default class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount(){
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0,4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatedPosts});
+                // console.log(response);
+            } )
+            .catch(error => {
+               console.log(error); 
+            //    this.setState({error: true});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render() {
+    let posts = <p style={{textAlign: 'center' }}>Something went wrong!</p>
+        if (!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    clicked={() => {this.postSelectedHandler(post.id)}}
+                    title={post.title} 
+                    key={post.id} 
+                    author={post.author}/>
+            });
+        }
+    return (
+        <section className="Posts">
+            {posts}
+        </section>
+    )
+  }
+}
