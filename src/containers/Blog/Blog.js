59457f7875b0a879e76adaba25f66b2ce284d091
Luis Rosales <eng.luisrosales@gmail.com>
2018-02-11 16:34:34 +0100

    using a state to render the selected post on the fullpost component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount(){
@@ -27,9 +28,17 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    } 
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post title={post.title} key={post.id} author={post.author}/>
+            return <Post 
+                clicked={() => {this.postSelectedHandler(post.id)}}
+                title={post.title} 
+                key={post.id} 
+                author={post.author}/>
         });
         return (
             <div>
@@ -37,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
